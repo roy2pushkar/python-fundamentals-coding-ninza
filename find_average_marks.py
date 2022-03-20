5fbfb3dde6485d8d027ca0e10ca73e0928d02fbd
roy2pushkar <roy2000pushkar.com>
2022-03-20 22:12:05 +0530

    Introduction to Python

---
+++ find_average_marks.py
@@ -0,0 +1,7 @@
+# Read input as sepcified in the question
+# Print output as specified in the question
+a=int(input())
+b=int(input())
+c=int(input())
+Average=(a+b+c)/3
+print(Average)
---
+++ rectangular_area.py
@@ -0,0 +1,6 @@
+# Write your code here
+a=int(input())
+b=int(input())
+c=int(input())
+diff=c-b
+print(diff)
